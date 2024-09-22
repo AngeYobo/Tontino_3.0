32c55f40a7080cfb8fc576781572e221d788f48d
Ange <docybo69@gmail.com>
2024-09-23 01:30:38 +0200

    Fix devShells for x86_64-linux

--- flake.nix
+++ flake.nix
@@ -5,21 +5,20 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";  # Use the unstable channel
   };
 
-  outputs = { self, nixpkgs }:
-    {
-      devShells = {
-        default = nixpkgs.lib.mkShell {
-          buildInputs = with nixpkgs; [
-            nodejs-18_x      # Node.js 18.x version
-            pnpm             # pnpm package manager
-            git              # Git for version control
-            aiken            # Aiken for smart contract development
-          ];
+  outputs = { self, nixpkgs }: {
+    devShells = {
+      x86_64-linux = nixpkgs.lib.mkShell {
+        buildInputs = with nixpkgs; [
+          nodejs-18_x      # Node.js 18.x version
+          pnpm             # pnpm package manager
+          git              # Git for version control
+          aiken            # Aiken for smart contract development
+        ];
 
-          shellHook = ''
-            echo "Welcome to the Tontino development environment!"
-          '';
-        };
+        shellHook = ''
+          echo "Welcome to the Tontino development environment!"
+        '';
       };
     };
+  };
 }
